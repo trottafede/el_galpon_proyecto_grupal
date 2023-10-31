e2c2f9a7b71bf6671fec1c9bd4e149f9b8c847b9
nahuelbonifacino <nahuelbonifacino98@gmail.com>
2023-10-31 01:10:14 -0300

    Logica en user.html

--- user.js
+++ user.js
@@ -1,26 +1,64 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const botonSorteo = document.getElementById("botonSorteo");
-  const form = document.getElementById("sorteos");
+  const botonSorteo = document.getElementById("ingresar");
+  const form = document.getElementById("form");
+  const info = document.getElementById("sorteos")
+  let sorteo = JSON.parse(localStorage.getItem("lista")) || [];
 
-  function mostrarInfo() {
-    form.innerHTML = ``;
-    form.innerHTML = `
-        <form>
-            <input type="text" name="" id="codigo"></input>
-            <button type="submit" id="ingresar">Ingregar codigo</button>
-        </form>
-        `;
-
-    const ingresarButton = document.getElementById("ingresar");
+  if (!sorteo == []){
+    info.innerHTML = ``
+    for(const user of sorteo){
+      info.innerHTML += `<p>${user.nombre} - ${user.participacion}</p>`
+    }
+  }
+   
+  function mostrarInfoSorteo() {  
+    const email = document.getElementById("inputEmail4");
+    const nombre = document.getElementById("inputNombre");
+    const apellido = document.getElementById("inputApellido");
+    const telefono = document.getElementById("inputNum");
+    const barcode = document.getElementById("barcode");
     const codigoInput = document.getElementById("codigo");
 
-    form.addEventListener("submit", (e) => {
-      e.preventDefault();
-      const valor_codigo = codigoInput.value;
-      alert(valor_codigo);
-    });
+    const user = {
+      participacion: codigoInput.value,
+      email: email.value,
+      nombre: nombre.value,
+      apellido: apellido.value,
+      telefono: telefono.value
+    }
+
+    const personaEncontrada = sorteo.find(user => user.participacion == codigoInput.value);
+    if(personaEncontrada){
+      alert("el codigo es invalido")
+    }else{
+      sorteo.push(user)
+      localStorage.setItem("lista", JSON.stringify(sorteo));
+
+      form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const valor_codigo = codigoInput.value;
+        JsBarcode(barcode, valor_codigo, {
+          format: "codabar",
+          lineColor: "#000",
+          width: 4,
+          height: 40,
+          displayValue: true
+        });
+      });
+    }
+
+    
+
+    info.innerHTML = ``
+    for(const user of sorteo){
+      info.innerHTML += `<p>${user.nombre} - ${user.participacion}</p>`
+    }
+ 
+    
   }
-  botonSorteo.addEventListener("click", mostrarInfo);
+  botonSorteo.addEventListener("click", mostrarInfoSorteo);
+
+
 });
 
 // es la idea, va tomando forma
